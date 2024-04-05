880aa12dacbafbc505400cd02a71720e5a0c48c0
kylee <kyleeharward21@gmail.com>
2024-04-05 17:43:00 -0600

    Switching to svelte!

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import paintIcon from "./assets/paint.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Grid from "./lib/Grid.svelte";
+</script>
+
+<main>
+  <!--left hand tools panel-->
+  <div class="tools">
+    
+  </div>
+  <div>
+    <Grid />
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+  }
+  .tools {
+    background-color: burlywood;
+  }
+</style>
---
+++ src/lib/Grid.svelte
@@ -0,0 +1,39 @@
+<script>
+  let size = 8;
+  let range = []
+  $: range = Array(size).fill().map((_, i) => i);
+</script>
+
+<!--each block to create a square grid based on size property-->
+<div class="gridInput">
+  <label for="size">Grid size:</label>
+  <input type="number" bind:value={size} min="1" max="10" />
+</div>
+<div class="grid" style="--size: {size};">
+  {#each range as row}
+    {#each range as column}
+      <div class="cell"></div>
+    {/each}
+  {/each}
+</div>
+
+<style>
+    .gridInput {
+      margin-bottom: 1rem;
+    }
+    .gridInput input {
+      padding: 0.25rem 0.5rem;
+      margin-left: 0.25rem;
+    }
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(var(--size), 1fr);
+      border: 1px solid black;
+      width: 400px; /* Set a fixed size for the grid */
+      height: 400px;
+    }
+    .cell {
+      border: 1px solid black;
+      padding: 10px;
+    }
+  </style>
